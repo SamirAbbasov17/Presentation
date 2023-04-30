a75a0cfae7aef4130973940de64724cced54e889
SamirAbbasov17 <abbasovsamir718@gmail.com>
2023-04-30 12:11:44 +0400

    Presentation Hazirlandi

---
+++ Presentation/LinkedListNodeExe/LinkedListNodeExe/LinkedList/Node.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedListNodeExe.LinkedList
+{
+    public class Node
+    {
+        public int Data { get; set; }
+        public Node Next { get; set; }
+
+        public void DisplayNode()
+        {
+            global::System.Console.WriteLine(Data);
+        }
+    }
+}
--- Presentation/LinkedListNodeExe/LinkedListNodeExe/Program.cs
+++ Presentation/LinkedListNodeExe/LinkedListNodeExe/Program.cs
@@ -1,36 +1,84 @@
 ﻿namespace LinkedListNodeExe
 {
+    using LinkedList;
     internal class Program
     {
         static void Main(string[] args)
         {
-            // Create a linked list of integers
-            LinkedList<int> list = new LinkedList<int>();
 
-            // Add some nodes to the list
-            LinkedListNode<int> node1 = list.AddLast(10);
-            LinkedListNode<int> node2 = list.AddLast(20);
-            LinkedListNode<int> node3 = list.AddLast(30);
+            #region GenericLinkedListExemple
 
-            // Traverse the list and print the elements
-            LinkedListNode<int> currentNode = list.First;
-            while (currentNode != null)
-            {
-                Console.WriteLine(currentNode.Value);
-                currentNode = currentNode.Next;
-            }
+                //EXEMPLE 1
+                    //// Create a linked list of integers
+                    //LinkedList<int> list = new LinkedList<int>();
 
-            // Remove the second node
-            list.Remove(node2);
+                    //// Add some nodes to the list
+                    //LinkedListNode<int> node1 = list.AddLast(10);
+                    //LinkedListNode<int> node2 = list.AddLast(20);
+                    //LinkedListNode<int> node3 = list.AddLast(30);
 
-            // Traverse the list again and print the elements
-            currentNode = list.First;
-            Console.WriteLine("Ikinci node silinenden sonra listin veziyyeti: ");
-            while (currentNode != null)
-            {
-                Console.WriteLine(currentNode.Value);
-                currentNode = currentNode.Next;
-            }
+                    //// Traverse the list and print the elements
+                    //LinkedListNode<int> currentNode = list.First;
+                    //while (currentNode != null)
+                    //{
+                    //    Console.WriteLine(currentNode.Value);
+                    //    currentNode = currentNode.Next;
+                    //}
+
+                    //// Remove the second node
+                    //list.Remove(node2);
+
+                    //// Traverse the list again and print the elements
+                    //currentNode = list.First;
+                    //Console.WriteLine("Ikinci node silinenden sonra listin veziyyeti: ");
+                    //while (currentNode != null)
+                    //{
+                    //    Console.WriteLine(currentNode.Value);
+                    //    currentNode = currentNode.Next;
+                    //}
+
+            #endregion
+
+            #region NodeExemple
+
+                //EXEMPLE 2
+
+                    //Node nodeA = new();
+                    //nodeA.Data = 865;
+
+                    //Node nodeB = new();
+                    //nodeB.Data = 344;
+
+                    //Node nodeC = new();
+                    //nodeC.Data = 888;
+
+                    //Node nodeD = new();
+                    //nodeD.Data = 222;
+
+                    //nodeA.Next = nodeB;
+                    //nodeB.Next = nodeC;
+                    //nodeC.Next = nodeD;
+
+            #endregion
+
+            #region LinkedListExemple
+                //EXEMPLE 3
+                    LinkedLists linkedList = new();
+                    linkedList.InsertFirst(1);
+                    linkedList.InsertFirst(2);
+                    linkedList.InsertFirst(3);
+                    linkedList.InsertFirst(4);
+
+                    linkedList.DeleteFirst();
+                    linkedList.DeleteFirst();
+
+                    linkedList.InsertLast(546);
+                    linkedList.InsertLast(3434);
+
+                    linkedList.DisplayList(); 
+            #endregion
+
+
         }
     }
 }
